b1384e484dbb07943ed015df813808d93f41b9f9
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-04-16 14:28:04 +0200

    created VillaController and Model

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    //[Route("api/VillaAPI")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new() { Id = 1, Name = "Pool View" },
+                new() { Id = 2, Name = "Beach View" }
+            };
+        }
+    }
+}
