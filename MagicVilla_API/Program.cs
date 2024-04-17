bb0cdd894db19c5c52ba2db78fe64408fb9229c8
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-04-17 15:16:58 +0200

    sample of use with serilog

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,6 +1,12 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Example of how to implement Serilog to log to a file instead of the built-in logger
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.File("log/villalogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
 
 builder.Services
     .AddControllers(option => { option.ReturnHttpNotAcceptable = true; })
