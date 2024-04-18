26c09ec31ed86beed9fcacc9b6655d9897b5807e
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-04-18 16:34:09 +0200

    added EF Core migration (add-migration and update-database)

--- MagicVilla_API/Data/ApplicationDbContext.cs
+++ MagicVilla_API/Data/ApplicationDbContext.cs
@@ -5,8 +5,8 @@ namespace MagicVilla_API.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option)
+            : base(option)
         {
         }
         public DbSet<Villa> Villas { get; set; }
