63532a5263ceb891b91699674acd776d4ae4ad4b
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-04-22 14:43:10 +0200

    created villa number repositories

---
+++ MagicVilla_API/Controllers/VillaNumberAPIController.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaNumberAPIController : ControllerBase
+    {
+        public VillaNumberAPIController(ApplicationDbContext db)
+        {
+            
+        }
+    }
+}
