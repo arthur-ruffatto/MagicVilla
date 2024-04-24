b028b61bdb49d12d5bda9b519687b2bef61f4437
Arthur Ruffatto <arthurruffatto@hotmail.com>
2024-04-24 14:32:17 +0200

    Created villa service and added to dependency injection

---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,65 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.DTO;
+using MagicVilla_Web.Services.IServices;
+using Utility;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly IHttpClientFactory httpClient;
+        private readonly string baseUrl;
+        public VillaService(IHttpClientFactory httpClient, IConfiguration config) : base(httpClient)
+        {
+            this.httpClient = httpClient;
+            baseUrl = config.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = dto,
+                Url = baseUrl + "api/VillaAPI/Create"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.DELETE,
+                Url = baseUrl + "api/VillaAPI/Delete/" + id
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = baseUrl + "api/VillaAPI/Get"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = baseUrl + "api/VillaAPI/Get/" + id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = StaticDetails.ApiType.PUT,
+                Data = dto,
+                Url = baseUrl + "api/VillaAPI/Update/" + dto.Id
+            });
+        }
+    }
+}
